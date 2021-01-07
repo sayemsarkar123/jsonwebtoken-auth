6fe940334f2c0c6c5cff3dacc6cfdaa114d99b0d
sayemsarkar123 <sayemsarkar123@gmail.com>
2021-01-07 20:54:55 +0600

    minor fixes

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const path = require('path');
 const logger = require('morgan');
 require('dotenv').config();
 
@@ -11,6 +10,8 @@ const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const shopsRouter = require('./routes/shops');
 
+const userController = require('./controllers/user.controller');
+
 const app = express();
 
 app.use(logger('dev'));
@@ -19,6 +20,6 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/shops', shopsRouter);
+app.use('/shops', userController.isAuthenticated, shopsRouter);
 
 module.exports = app;
--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,6 @@ const router = express.Router();
 
 const userController = require('../controllers/user.controller');
 
-router.get('/', userController.isAuthenticated);
 router.post('/login', userController.login);
 router.post('/register', userController.register);
 
